d876e820f0b3c705512c23c707d821c8bbaf2c20
Jens Carlén <jens@neubeck.se>
2015-01-21 08:17:41 +0100

    Clustering

---
+++ cluster.js
@@ -0,0 +1,33 @@
+var config = require("./config");
+var cluster = require("cluster");
+var os = require("os");
+var logger = require("./lib/logger.js");
+var packageInfo = require("./package.json");
+
+cluster.setupMaster({
+  exec : __dirname
+});
+
+var workerCount = config.boolean("cluster") ? config.clusterWorkers || os.cpus().length : 1;
+logger.info("Starting %s cluster with %d workers", packageInfo.name, workerCount);
+for (var i = 0; i < workerCount; i++) {
+  var originalId = i + 1;
+  var worker = cluster.fork(workerEnv(originalId));
+  worker.originalId = originalId;
+}
+
+cluster.on("exit", function (deadWorker, code, signal) {
+  logger.warning("Worker %s died (code: %d, signal: %s, originalId: %d)", deadWorker.process.pid, code, signal, deadWorker.originalId);
+
+  var worker = cluster.fork(workerEnv(deadWorker.originalId));
+  worker.originalId = deadWorker.originalId;
+
+  logger.info("Worker %s was born (originalId: %d)", worker.process.pid, deadWorker.originalId);
+});
+
+function workerEnv(originalId) {
+  return {
+    START_LISTENER: "true",
+    NODE_TIME: originalId <= 2
+  };
+}
