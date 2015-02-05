bdaf856f3987b0fc189a21274033d45721b2667a
Gabriel Falkenberg <gabriel.falkenberg@gmail.com>
2015-02-05 09:22:56 +0100

    Add _revision which serves the app's git revision.

---
+++ test/feature/alive-feature.js
@@ -0,0 +1,52 @@
+// jshint newcap: false
+
+var fs = require("fs");
+var path = require("path");
+var request = require("supertest");
+var app = require("../../");
+
+Feature("_alive", function () {
+  var aliveFilePath = path.join(__dirname, "..", "..", "config", "alive");
+
+  Scenario("Basic alive check", function () {
+    Given("The file config/alive exists", function (done) {
+      fs.exists(aliveFilePath, function (exists) {
+        if (!exists) return done(new Error(aliveFilePath + " does not exist!"));
+
+        return done();
+      });
+    });
+    When("Requesting /_alive will return 200 Yes", function (done) {
+      request(app)
+        .get("/_alive")
+        .expect(200)
+        .expect("Yes")
+        .end(done);
+    });
+  });
+
+  Scenario("Preparing to shutdown", function () {
+    var fileData;
+    before(function (done) {
+      fs.readFile(aliveFilePath, function (err, data) {
+        if (err) return done(err);
+        fileData = data;
+        done();
+      });
+    });
+    after(function (done) {
+      fs.writeFile(aliveFilePath, fileData, done);
+    });
+
+    Given("The file config/alive has been removed", function (done) {
+      fs.unlink(aliveFilePath, done);
+    });
+    When("Requesting /_alive will return 404 No", function (done) {
+      request(app)
+        .get("/_alive")
+        .expect(404)
+        .expect("No")
+        .end(done);
+    });
+  });
+});
---
+++ test/feature/revision-feature.js
@@ -0,0 +1,35 @@
+// jshint newcap: false
+
+var fs = require("fs");
+var path = require("path");
+var request = require("supertest");
+var app = require("../../");
+
+Feature("_revision", function () {
+  var revisionFilePath = path.join(__dirname, "..", "..", "config", "revision");
+
+  Scenario("Getting revision when it is there", function () {
+    Given("The file config/revision exists", function (done) {
+      fs.writeFile(revisionFilePath, "some-revision", done);
+    });
+    When("Requesting /_revision will return 200 and the revision from the file", function (done) {
+      request(app)
+        .get("/_revision")
+        .expect(200)
+        .expect("some-revision")
+        .end(done);
+    });
+  });
+
+  Scenario("Getting revision when it is not there", function () {
+    Given("The file config/revision has been removed", function (done) {
+      fs.unlink(revisionFilePath, done);
+    });
+    When("Requesting /_revision will return 404 Not Found", function (done) {
+      request(app)
+        .get("/_revision")
+        .expect(404)
+        .end(done);
+    });
+  });
+});
