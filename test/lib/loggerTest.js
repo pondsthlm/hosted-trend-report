a29c6a81e5c47a12a527838c6349f252ab842e36
Gabriel Falkenberg <gabriel.falkenberg@gmail.com>
2015-02-18 21:19:48 +0100

    Logger works and logs to file during tests to avoid littering mocha output.

---
+++ test/lib/loggerTest.js
@@ -0,0 +1,9 @@
+var fs = require("fs");
+var logger = require("../../lib/logger.js");
+
+describe("logger", function () {
+  it("logs to file in tests", function () {
+    logger.debug("testing logger");
+    fs.readFileSync(__dirname + "/../../logs/test.log", "utf-8").should.include("testing logger");
+  });
+});
