aa3f8792b6bd960afdc7fdec68b521b12614b850
Daniel Sundman <daniel@yds.se>
2015-03-26 10:03:32 +0100

    added client specific .jshintrc

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,8 +1,9 @@
-"use strict";
-
 // Put your client side javascript in this directory and its subdirectories
 
 (function () {
+
+  "use strict";
+
   function foo() {
     console.log("Winning!");
   }
