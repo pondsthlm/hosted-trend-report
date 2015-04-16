16ea1fc771b957c21d96a6446571829042c1a5d9
Jens Carlén <jens@neubeck.se>
2015-04-16 13:33:48 +0200

    Made logger test async

--- test/lib/loggerTest.js
+++ test/lib/loggerTest.js
@@ -4,8 +4,13 @@ var fs = require("fs");
 var logger = require("../../lib/logger.js");
 
 describe("logger", function () {
-  it("logs to file in tests", function () {
+  it("logs to file in tests", function (done) {
     logger.debug("testing logger");
-    fs.readFileSync(__dirname + "/../../logs/test.log", "utf-8").should.include("testing logger");
+
+    setImmediate(function () {
+      fs.readFileSync(__dirname + "/../../logs/test.log", "utf-8").should.include("testing logger");
+      done();
+    });
   });
 });
+
