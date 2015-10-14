682fa7f7ce094b281357a03a55bfcb8cbf73d4a9
Ricard Fredin <ricard.fredin@valtech.se>
2015-10-14 08:58:23 +0200

    Fixes eslint warnings.

--- cluster.js
+++ cluster.js
@@ -5,7 +5,6 @@ var cluster = require("cluster");
 var os = require("os");
 var logger = require("./lib/logger.js");
 var packageInfo = require("./package.json");
-var worker, originalId;
 
 cluster.setupMaster({
   exec: __dirname
@@ -14,22 +13,20 @@ cluster.setupMaster({
 var workerCount = config.boolean("cluster") ? config.clusterWorkers || os.cpus().length : 1;
 logger.info("Starting %s cluster with %d workers", packageInfo.name, workerCount);
 for (var i = 0; i < workerCount; i++) {
-  originalId = i + 1;
-  worker = cluster.fork(workerEnv(originalId));
-  worker.originalId = originalId;
+  cluster.fork(workerEnv(i + 1)).originalId = i + 1;
 }
 
 cluster.on("exit", function (deadWorker, code, signal) {
   logger.warning("Worker %s died (code: %d, signal: %s, originalId: %d)", deadWorker.process.pid, code, signal, deadWorker.originalId);
 
-  worker = cluster.fork(workerEnv(deadWorker.originalId));
+  var worker = cluster.fork(workerEnv(deadWorker.originalId));
   worker.originalId = deadWorker.originalId;
 
   logger.info("Worker %s was born (originalId: %d)", worker.process.pid, deadWorker.originalId);
 });
 
-function workerEnv(origId) {
+function workerEnv(originalId) {
   return {
-    NODE_TIME: origId <= 2
+    NODE_TIME: originalId <= 2
   };
 }
