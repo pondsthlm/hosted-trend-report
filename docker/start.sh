c58eef18583e56b3395dd75bc0827fed7f1238f6
Mattias Nordberg <mattias.nordberg@gmail.com>
2015-12-04 16:23:39 +0100

    run pm2 as web user

--- docker/start.sh
+++ docker/start.sh
@@ -6,25 +6,25 @@
 # - Drop in execs in /exp-container/exec to make pm2 fork them on start (e.g varnish/consul-template)
 echo "Starting exp-container for fun and profit."
 if [ -d /exp-container/exec ]; then
+  chown -R web:web /home/web
   chown -R web:web /exp-container/data
   for x in $(find /exp-container/exec -type f); do
     xup=$(basename $x | awk '{print toupper($0)}')
     enabled=$(eval "echo \$${xup%.*}_ENABLED")
-    user=$(eval "echo \$${xup%.*}_USER")
     if [ "${enabled}" = true ]; then
       echo "Starting $x..."
       if [ "$xup" = "PM2_WEB.SH" ]; then
-        "${x}"
+        su -c "${x}" -s /bin/bash - web
       else
-        NODE_ENV=${NODE_ENV:-production} pm2 -u ${user:-root} start "${x}"
+        su -c "NODE_ENV=${NODE_ENV:-production} pm2 start ${x}" -s /bin/bash - web
       fi
     fi
   done
   # This is our long living CMD
   if [ "${NODE_ENV}" = "development" ]; then
-    exec pm2 logs --no-color
+    exec su -c "pm2 logs" -s /bin/bash - web
   else
-    exec tail -f /root/.pm2/pm2.log
+    exec tail -f /home/web/.pm2/pm2.log
   fi
 else
   echo "Nothing to run"
