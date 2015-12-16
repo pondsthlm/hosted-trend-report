f7c8d6fb04cdde5f2fc8ab761382b9ffec71a2c9
Mattias Nordberg <mattias.nordberg@gmail.com>
2015-12-16 13:08:09 +0100

    fix varnish config variable replace

--- docker/exec/varnish.sh
+++ docker/exec/varnish.sh
@@ -5,9 +5,10 @@ VARNISH_BACKEND_PORT=${VARNISH_BACKEND_PORT:-3000}
 VARNISH_BACKEND_IP=${VARNISH_BACKEND_IP:-127.0.0.1}
 VARNISH_PORT=${VARNISH_PORT:-80}
 
+cp $VARNISH_CONF /exp-container/varnish/generated.vcl
 for name in VARNISH_BACKEND_PORT VARNISH_BACKEND_IP
 do
     eval value=\$$name
-    sed "s|\${${name}}|${value}|g" $VARNISH_CONF > /exp-container/varnish/generated.vcl
+    sed -i "s|\${${name}}|${value}|g" /exp-container/varnish/generated.vcl
 done
 varnishd -f /exp-container/varnish/generated.vcl -s malloc,250M -a 0.0.0.0:${VARNISH_PORT} -u web -F -n /exp-container/varnish
