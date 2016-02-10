8d2fc2a0850277054ed2d2dd19df505526d9034b
Mattias Nordberg <mattias.nordberg@gmail.com>
2016-02-10 16:55:27 +0100

    update node versions

--- docker/build.sh
+++ docker/build.sh
@@ -1,50 +1,46 @@
 #!/bin/bash -e
 
+docker pull alpine:3.2
+docker pull centos:7
+
 # Node.js 0.10 Alpine
 docker pull exp-docker.repo.dex.nu/nodejs:0.10 || true
 docker build -f Dockerfile.nodejs-0.10 -t exp-docker.repo.dex.nu/nodejs:0.10 .
-docker tag -f exp-docker.repo.dex.nu/nodejs:0.10 exp-docker.repo.dex.nu/nodejs:0.10.40
+docker tag -f exp-docker.repo.dex.nu/nodejs:0.10 exp-docker.repo.dex.nu/nodejs:0.10.42
 docker push exp-docker.repo.dex.nu/nodejs:0.10
-docker push exp-docker.repo.dex.nu/nodejs:0.10.40
+docker push exp-docker.repo.dex.nu/nodejs:0.10.42
 
 # Node.js 0.12 Alpine
 docker pull exp-docker.repo.dex.nu/nodejs:0.12 || true
 docker build -f Dockerfile.nodejs-0.12 -t exp-docker.repo.dex.nu/nodejs:0.12 .
-docker tag -f exp-docker.repo.dex.nu/nodejs:0.12 exp-docker.repo.dex.nu/nodejs:0.12.7
+docker tag -f exp-docker.repo.dex.nu/nodejs:0.12 exp-docker.repo.dex.nu/nodejs:0.12.10
 docker push exp-docker.repo.dex.nu/nodejs:0.12
-docker push exp-docker.repo.dex.nu/nodejs:0.12.7
-
-# Node.js 0.10 CentOS
-docker pull exp-docker.repo.dex.nu/nodejs:0.10-centos || true
-docker build -f Dockerfile.nodejs-0.10-centos -t exp-docker.repo.dex.nu/nodejs:0.10-centos .
-docker tag -f exp-docker.repo.dex.nu/nodejs:0.10-centos exp-docker.repo.dex.nu/nodejs:0.10.40-centos
-docker push exp-docker.repo.dex.nu/nodejs:0.10-centos
-docker push exp-docker.repo.dex.nu/nodejs:0.10.40-centos
+docker push exp-docker.repo.dex.nu/nodejs:0.12.10
 
 # Node.js 0.12 CentOS
 docker pull exp-docker.repo.dex.nu/nodejs:0.12-centos || true
 docker build -f Dockerfile.nodejs-0.12-centos -t exp-docker.repo.dex.nu/nodejs:0.12-centos .
-docker tag -f exp-docker.repo.dex.nu/nodejs:0.12-centos exp-docker.repo.dex.nu/nodejs:0.12.7-centos
+docker tag -f exp-docker.repo.dex.nu/nodejs:0.12-centos exp-docker.repo.dex.nu/nodejs:0.12.10-centos
 docker push exp-docker.repo.dex.nu/nodejs:0.12-centos
-docker push exp-docker.repo.dex.nu/nodejs:0.12.7-centos
+docker push exp-docker.repo.dex.nu/nodejs:0.12.10-centos
 
 # Node.js 4 Alpine
 docker pull exp-docker.repo.dex.nu/nodejs:4 || true
 docker build -f Dockerfile.nodejs-4 -t exp-docker.repo.dex.nu/nodejs:4 .
-docker tag -f exp-docker.repo.dex.nu/nodejs:4 exp-docker.repo.dex.nu/nodejs:4.2
-docker tag -f exp-docker.repo.dex.nu/nodejs:4 exp-docker.repo.dex.nu/nodejs:4.2.2
+docker tag -f exp-docker.repo.dex.nu/nodejs:4 exp-docker.repo.dex.nu/nodejs:4.3
+docker tag -f exp-docker.repo.dex.nu/nodejs:4 exp-docker.repo.dex.nu/nodejs:4.3.0
 docker push exp-docker.repo.dex.nu/nodejs:4
-docker push exp-docker.repo.dex.nu/nodejs:4.2
-docker push exp-docker.repo.dex.nu/nodejs:4.2.2
+docker push exp-docker.repo.dex.nu/nodejs:4.3
+docker push exp-docker.repo.dex.nu/nodejs:4.3.0
 
 # Node.js 4 CentOS
 docker pull exp-docker.repo.dex.nu/nodejs:4-centos || true
 docker build -f Dockerfile.nodejs-4-centos -t exp-docker.repo.dex.nu/nodejs:4-centos .
-docker tag -f exp-docker.repo.dex.nu/nodejs:4-centos exp-docker.repo.dex.nu/nodejs:4.2-centos
-docker tag -f exp-docker.repo.dex.nu/nodejs:4-centos exp-docker.repo.dex.nu/nodejs:4.2.2-centos
+docker tag -f exp-docker.repo.dex.nu/nodejs:4-centos exp-docker.repo.dex.nu/nodejs:4.3-centos
+docker tag -f exp-docker.repo.dex.nu/nodejs:4-centos exp-docker.repo.dex.nu/nodejs:4.3.0-centos
 docker push exp-docker.repo.dex.nu/nodejs:4-centos
-docker push exp-docker.repo.dex.nu/nodejs:4.2-centos
-docker push exp-docker.repo.dex.nu/nodejs:4.2.2-centos
+docker push exp-docker.repo.dex.nu/nodejs:4.3-centos
+docker push exp-docker.repo.dex.nu/nodejs:4.3.0-centos
 
 # Node.js 5 Alpine
 docker pull exp-docker.repo.dex.nu/nodejs:5 || true
@@ -58,9 +54,9 @@ docker push exp-docker.repo.dex.nu/nodejs:5.6.0
 # Node.js 5 CentOS
 docker pull exp-docker.repo.dex.nu/nodejs:5-centos || true
 docker build -f Dockerfile.nodejs-5-centos -t exp-docker.repo.dex.nu/nodejs:5-centos .
-docker tag -f exp-docker.repo.dex.nu/nodejs:5-centos exp-docker.repo.dex.nu/nodejs:5.4-centos
-docker tag -f exp-docker.repo.dex.nu/nodejs:5-centos exp-docker.repo.dex.nu/nodejs:5.4.0-centos
+docker tag -f exp-docker.repo.dex.nu/nodejs:5-centos exp-docker.repo.dex.nu/nodejs:5.6-centos
+docker tag -f exp-docker.repo.dex.nu/nodejs:5-centos exp-docker.repo.dex.nu/nodejs:5.6.0-centos
 docker push exp-docker.repo.dex.nu/nodejs:5-centos
-docker push exp-docker.repo.dex.nu/nodejs:5.4-centos
-docker push exp-docker.repo.dex.nu/nodejs:5.4.0-centos
+docker push exp-docker.repo.dex.nu/nodejs:5.6-centos
+docker push exp-docker.repo.dex.nu/nodejs:5.6.0-centos
 
