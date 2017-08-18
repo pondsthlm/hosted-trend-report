577209936549f153349cde165128ab9b90c38e6f
Dogan Yazar <dogan.yazar@expressen.se>
2017-08-18 15:58:16 +0200

    Hlsjs example

--- app/main.js
+++ app/main.js
@@ -1,15 +1,15 @@
-import foo from "./foo.js";
-import { combineReducers } from "redux";
+import Hls from "hls.js/dist/hls.light.min.js";
 
-function x() {
-  return Promise.resolve(5);
+function run () {
+  if (Hls.isSupported()) {
+    const video = document.getElementById("video");
+    const hls = new Hls();
+    hls.loadSource("http://httpcache0.03837-cachelive2.dna.qbrick.com/03837-cachelive2/smil:03837_tx2_720p/playlist.m3u8");
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, () => {
+      video.play();
+    });
+  }
 }
 
-async function y() {
-  await x();
-}
-
-export default function () {
-  combineReducers({});
-  y().then(() => console.log(foo));
-}
+run();
