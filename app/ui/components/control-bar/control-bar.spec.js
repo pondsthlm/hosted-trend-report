fa29fcca56da05145c60349453bdd01403f36e82
Anton Björklund <bierklund@gmail.com>
2017-08-31 20:57:05 +0200

    add actual tests

--- app/ui/components/control-bar/control-bar.spec.js
+++ app/ui/components/control-bar/control-bar.spec.js
@@ -1,6 +1,7 @@
 import {defaultVideoState} from "../../reducer";
 import controlBar from "./control-bar.js";
 import { div } from "../../../helpers/make-element";
+import assert from "assert";
 
 
 //dom.querySelector("").click();
@@ -16,7 +17,18 @@ describe("Loding controlbar with defaltstate", () => {
   }
   const dom = controlBar(state, dispatchCallback);
   console.log("querySelector test", dom.querySelector("button").innerHTML);
-  it("", (done) => {
 
+  it("the className should be control-bar", () => {
+    dom.className.should.equal("control-bar");
+  });
+
+  it("should have four components", () => {
+    dom.children.length.should.equal(4);
+  });
+
+  it("the components should have the right bem parent className", () => {
+    [].forEach.call(dom.children, (componentNode) => {
+      assert.equal(true, componentNode.className.startsWith("control-bar"));
+    });
   });
 });
