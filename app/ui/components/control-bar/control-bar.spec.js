9f5975b35e3ec36de61be339ee4c4497ec5421b6
Anton Björklund <bierklund@gmail.com>
2017-08-31 22:47:25 +0200

    add an dispatch example/test

--- app/ui/components/control-bar/control-bar.spec.js
+++ app/ui/components/control-bar/control-bar.spec.js
@@ -1,22 +1,15 @@
 import {defaultVideoState} from "../../reducer";
-import controlBar from "./control-bar.js";
+import controlBar from "./control-bar";
+import play from "./play";
 import { div } from "../../../helpers/make-element";
 import assert from "assert";
 
+// Default state needs an element.
+defaultVideoState.elementContainer = div();
 
-//dom.querySelector("").click();
-
-describe("Loding controlbar with defaltstate", () => {
-  const state = Object.assign({}, defaultVideoState, {
-    elementContainer: div()
-  });
-  const dispatches = [];
-
-  function dispatchCallback(object) {
-    dispatches.push(object);
-  }
-  const dom = controlBar(state, dispatchCallback);
-  console.log("querySelector test", dom.querySelector("button").innerHTML);
+describe("Loading controlbar with default state", () => {
+  const state = defaultVideoState;
+  const dom = controlBar(state);
 
   it("the className should be control-bar", () => {
     dom.className.should.equal("control-bar");
@@ -30,5 +23,41 @@ describe("Loding controlbar with defaltstate", () => {
     [].forEach.call(dom.children, (componentNode) => {
       assert.equal(true, componentNode.className.startsWith("control-bar"));
     });
+  });
+});
+
+describe("Loading play with default state", () => {
+  const state = defaultVideoState;
+  const dispatches = [];
+
+  function dispatchCallback(object) {
+    dispatches.push(object);
+  }
+
+  const dom = play(state, dispatchCallback);
+  it("should have a dispatch play when clicked", () => {
+    dispatches.length.should.equal(0);
+    dom.click();
+    dispatches.length.should.equal(1);
+    dispatches[0].type.should.equal("PLAY");
+  });
+});
+
+describe("Loading play with isPLaying state", () => {
+  const state = Object.assign({}, defaultVideoState, {
+    isPlaying: true
+  });
+  const dispatches = [];
+
+  function dispatchCallback(object) {
+    dispatches.push(object);
+  }
+
+  const dom = play(state, dispatchCallback);
+  it("should have a dispatch pause when clicked", () => {
+    dispatches.length.should.equal(0);
+    dom.click();
+    dispatches.length.should.equal(1);
+    dispatches[0].type.should.equal("PAUSE");
   });
 });
