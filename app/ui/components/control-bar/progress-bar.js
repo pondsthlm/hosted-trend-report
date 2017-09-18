3aa9e2036ba5eee00b90e180d92c1d081a9c4efd
Anton Björklund <bierklund@gmail.com>
2017-09-18 13:15:30 +0200

    cleanup progress-bar.js

--- app/ui/components/control-bar/progress-bar.js
+++ app/ui/components/control-bar/progress-bar.js
@@ -3,54 +3,65 @@ import actions from "../../../player/actions";
 
 import "./progress-bar.styl";
 
-let bemParent = "";
-const className = "progress-bar";
+
+function progressBar(state, dispatch, parentClassName) {
+  const className = "progress-bar";
+  const bemParent = parentClassName ? `${parentClassName}__` : "";
+
+  return div(
+    {className: `${bemParent}${className} ${className}`},
+    scrubber(state, dispatch, className),
+  );
+}
 
 function calculateProgressPercent(state) {
   return `${(state.currentTime / state.duration) * 100}%`;
 }
 
-function progressBar(state, dispatch, parentClassName) {
-  if (parentClassName) {
-    bemParent = `${parentClassName}__`;
-  }
-  const progress = div(
+function scrubber(state, dispatch, parentClassName) {
+
+  const dom = div(
     {
-      className: `${className}__progress`,
-      style: {
-        width: calculateProgressPercent(state)
-      }
-    }
-  );
-
-  progress.update((state) => {
-    const object = {
-      style: {
-        width: calculateProgressPercent(state)
-      }
-    };
-
-    return object;
-  });
-
-  const scrubber = div(
-    {
-      className: `${className}__scrubber`,
+      className: `${parentClassName}__scrubber`,
       onmousedown: (event) => {
         const percent = event.offsetX / scrubber.offsetWidth;
         const newTime = percent * state.duration;
-        console.log(newTime, state.duration, event.offsetX, scrubber.offsetWidth);
         dispatch(actions.setTime(newTime));
       }
-    }, progress);
-
-
-  return div(
-    {
-      className: `${bemParent}${className} ${className}`,
     },
-    scrubber,
+    progress(state, dispatch, parentClassName)
   );
+
+  dom.update((newState) => {
+    return {
+      onmousedown: (event) => {
+        const percent = event.offsetX / scrubber.offsetWidth;
+        const newTime = percent * newState.duration;
+        dispatch(actions.setTime(newTime));
+      }
+    };
+  });
+
+  return dom;
 }
+
+function progress(state, dispatch, parentClassName) {
+
+  const dom = div({
+    className: `${parentClassName}__progress`,
+    style: {
+      width: calculateProgressPercent(state)
+    }
+  });
+
+  dom.update((newState) => {
+    return {
+      style: {
+        width: calculateProgressPercent(newState)
+      }
+    };
+  });
+}
+
 
 export default progressBar;
